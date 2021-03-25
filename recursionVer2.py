077f6900316f9ed52368141d717bae753d38e11e
Cmeacham <65180790+Cmeacham@users.noreply.github.com>
2021-03-25 15:12:51 -0400

    Update recursionVer2.py

--- recursionVer2.py
+++ recursionVer2.py
@@ -1,4 +1,4 @@
-# Name: Christopher Meacham
+# Name: Christopher Meacham and Mary-Margaret West
 # Subject: SY402 Lab 08 Version 2
 # Description: This is a python file that utilizes recursion in order to sort through multiple file directories. The libraries used are the os, datetime, and sys libraries. The file takes in input from the user's command line for the desired directory. The file then utilizes the os library by making a list of the 1st level directories and files in a given location (os.listdir()) and then checking later on whether or not the values in the directory list (from os.listdir()) is a file or a directory. If the value is a file, then it will print out the file name, its directory, and the time at which this was done (utilizing the datetime library). If the value is a directory, the function will call itself, but this time, will use the original directory with the appended directory value at the end of it. Essentially going through the same process, but within one more level into a directory. There are certain directories that should not be looked into, those are listed after the value is detected to be a directory.
 
